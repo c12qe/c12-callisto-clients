3bc390e0147d477a31d9dcd3795a3a6d0caf2c30
rviktor85 <119857613+rviktor85@users.noreply.github.com>
2024-03-20 10:56:44 +0100

    fix: conf.py change

--- docs/conf.py
+++ docs/conf.py
@@ -64,44 +64,43 @@ html_css_files = [
 #     "display_version": True,
 # }
 
-html_context = {
-    "current_version": "0.0.6",
-    "versions": [["0.0.6", "link"], ["test", "link to test"]],
-    "current_language": "en",
-    "languages": [["en", "link to en"]],
-}
-
-
-#
-# current_language = os.environ.get("current_language")
-# current_version = os.environ.get("current_version")
-#
 # html_context = {
-#     "current_language": current_language,
-#     "languages": [],
-#     "current_version": current_version,
-#     "versions": [],
+#     "current_version": "0.0.6",
+#     "versions": [["0.0.6", "link"], ["test", "link to test"]],
+#     "current_language": "en",
+#     "languages": [["en", "link to en"]],
 # }
-# pages_root = os.environ.get("pages_root", "")
-#
-#
-# if current_version == "latest":
-#     html_context["languages"].append(["en", pages_root])
-#     html_context["languages"].append(["de", pages_root + "/de"])
-#
-# if current_language == "en":
-#     html_context["versions"].append(["latest", pages_root])
-#
-# # and loop over all other versions from our yaml file
-# # to set versions and languages
-# with open("versions.yaml", "r") as yaml_file:
-#     docs = yaml.safe_load(yaml_file)
-#
-# if current_version != "latest":
-#     for language in docs[current_version].get("languages", []):
-#         html_context["languages"].append(
-#             [language, pages_root + "/" + current_version + "/" + language]
-#         )
-#
-# for version, details in docs.items():
-#     html_context["versions"].append([version, pages_root + "/" + version + "/" + current_language])
+
+
+current_language = os.environ.get("current_language")
+current_version = os.environ.get("current_version")
+
+html_context = {
+    "current_language": current_language,
+    "languages": [],
+    "current_version": current_version,
+    "versions": [],
+}
+pages_root = os.environ.get("pages_root", "")
+
+
+if current_version == "latest":
+    html_context["languages"].append(["en", pages_root])
+    html_context["languages"].append(["de", pages_root + "/de"])
+
+if current_language == "en":
+    html_context["versions"].append(["latest", pages_root])
+
+# and loop over all other versions from our yaml file
+# to set versions and languages
+with open("versions.yaml", "r") as yaml_file:
+    docs = yaml.safe_load(yaml_file)
+
+if current_version != "latest":
+    for language in docs[current_version].get("languages", []):
+        html_context["languages"].append(
+            [language, pages_root + "/" + current_version + "/" + language]
+        )
+
+for version, details in docs.items():
+    html_context["versions"].append([version, pages_root + "/" + version + "/" + current_language])
