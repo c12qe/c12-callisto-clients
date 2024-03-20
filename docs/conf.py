3d64cc380e593c4903d7fa6dfc505e8101bec7e8
rviktor85 <119857613+rviktor85@users.noreply.github.com>
2024-03-20 10:25:47 +0100

    feat: add docs change

--- docs/conf.py
+++ docs/conf.py
@@ -15,10 +15,10 @@ import sys
 default_path = os.path.dirname(__file__)
 sys.path.insert(0, os.path.abspath(default_path + "/../src"))
 
-project = "c12-callisto-clients"
+project = "CALLISTO"
 copyright = "2024, C12 Quantum Electronics"
 author = "C12 Quantum Electronics"
-release = "0.0.6"
+release = "2.01"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -33,50 +33,74 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_logo = "logo.png"
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
+html_favicon = "_static/favicon.png"
 html_theme_options = {
-    "display_version": True,
+    "announcement": "We're pleased to announce that <bold>CALLISTO 2.01</bold> is now released!",
+    "light_css_variables": {
+        "color-brand-primary": "#3A3938",
+        "color-brand-content": "#D6A018",
+        "color-brand-visited": "#A09D98",
+        "font-stack": "Formular, Cardone Micro Trial, Inconsolata, monospace",
+        "font-stack--monospace": "Fira Code, Courier, monospace",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#C6E5E1",
+        "color-brand-content": "#D6A018",
+        "color-brand-visited": "#A09D98",
+        "font-stack": "Formular, Cardone Micro Trial, Inconsolata, monospace",
+        "font-stack--monospace": "Fira Code, Courier, monospace",
+    },
+    "light_logo": "logo_black.svg",
+    "dark_logo": "logo_white.svg",
 }
 
-# html_context = {
-#     "current_version": "0.0.6",
-#     "versions": [["0.0.6", "link"], ["test", "link to test"]],
-#     "current_language": "en",
-#     "languages": [["en", "link to en"]],
+html_css_files = [
+    "css/custom.css",
+]
+
+# html_theme_options = {
+#     "display_version": True,
 # }
 
-
-current_language = os.environ.get("current_language")
-current_version = os.environ.get("current_version")
-
 html_context = {
-    "current_language": current_language,
-    "languages": [],
-    "current_version": current_version,
-    "versions": [],
+    "current_version": "0.0.6",
+    "versions": [["0.0.6", "link"], ["test", "link to test"]],
+    "current_language": "en",
+    "languages": [["en", "link to en"]],
 }
-pages_root = os.environ.get("pages_root", "")
 
-
-if current_version == "latest":
-    html_context["languages"].append(["en", pages_root])
-    html_context["languages"].append(["de", pages_root + "/de"])
-
-if current_language == "en":
-    html_context["versions"].append(["latest", pages_root])
-
-# and loop over all other versions from our yaml file
-# to set versions and languages
-with open("versions.yaml", "r") as yaml_file:
-    docs = yaml.safe_load(yaml_file)
-
-if current_version != "latest":
-    for language in docs[current_version].get("languages", []):
-        html_context["languages"].append(
-            [language, pages_root + "/" + current_version + "/" + language]
-        )
-
-for version, details in docs.items():
-    html_context["versions"].append([version, pages_root + "/" + version + "/" + current_language])
+#
+# current_language = os.environ.get("current_language")
+# current_version = os.environ.get("current_version")
+#
+# html_context = {
+#     "current_language": current_language,
+#     "languages": [],
+#     "current_version": current_version,
+#     "versions": [],
+# }
+# pages_root = os.environ.get("pages_root", "")
+#
+#
+# if current_version == "latest":
+#     html_context["languages"].append(["en", pages_root])
+#     html_context["languages"].append(["de", pages_root + "/de"])
+#
+# if current_language == "en":
+#     html_context["versions"].append(["latest", pages_root])
+#
+# # and loop over all other versions from our yaml file
+# # to set versions and languages
+# with open("versions.yaml", "r") as yaml_file:
+#     docs = yaml.safe_load(yaml_file)
+#
+# if current_version != "latest":
+#     for language in docs[current_version].get("languages", []):
+#         html_context["languages"].append(
+#             [language, pages_root + "/" + current_version + "/" + language]
+#         )
+#
+# for version, details in docs.items():
+#     html_context["versions"].append([version, pages_root + "/" + version + "/" + current_language])
--- docs/index.rst
+++ docs/index.rst
@@ -3,22 +3,68 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-C12 Callisto Emulator
-================================================
 
-C12 emulator is the first quantum emulator of the future C12 quantum computers. Based on experimentally measured C12 qubit, it offers an emulator with more qubits and more availability than our quantum processors in development. It provides tools for composing quantum circuits, running simulations and comparing both the ideal and realistic outcomes.
+Introduction
+====================================
 
-It aims to be a companion tool for developing gate-based quantum circuits, testing them in realistic environment & studying their scaling law.
+c12-callisto-clients is a Python package with tools that enable a user to communicate with C12's Callisto emulator, whose purpose is to emulate operations on the C12's quantum computer hardware.
 
-If you find any bugs or if you have any suggestions feel free to send us a mail to emulator@c12qe.com.
+C12’s quantum computer is based on optimized spin qubits. The spin qubit is realized from electrons trapped in a double quantum dot suspended on a carbon nanotube embedded in a silicon nanofabricated circuit. C12’s spin qubits have high fidelity, scalability and connectivity because of the properties of the materials used.
 
-We are so happy and proud to let you experiment with a software that imitates new generation of quantum computers.
+Installation
+====================================
 
+To use Callisto, you design quantum circuits according to either `Qiskit <https://docs.quantum.ibm.com/>`_ or `OpenQASM <https://openqasm.com/>`_ specifications and send them as jobs to the Callisto quantum emulator.
+
+This is done via `Python <https://www.python.org/>`_, so the first step is to install Python and/or an Integrated Development Environment such as `PyCharm <https://www.jetbrains.com/pycharm/>`_ or `Visual Studio Code <https://code.visualstudio.com/>`_. Then install the Callisto client.
+
+From the PyPI repository
+------------------------
+
+Run the following command inside your local Python environment:
+
+.. code-block:: bash
+
+   pip install c12_callisto_clients
+
+From the GitHub package
+-----------------------
+
+In order to run the package, the best approach is to create a Conda environment where all the necessary packages will be installed. To do that, we need to have Conda installed (if that is not the case see `Conda installation <https://conda.io/projects/conda/en/latest/user-guide/install/index.html>`_).
+
+Clone the GitHub repository into local folder:
+
+.. code-block:: bash
+
+   git clone https://github.com/c12qe/c12simulator-clients.git
+
+Create the Conda environment with the command:
+
+.. code-block:: bash
+
+   conda env create -f environment.yml
+
+Then activate the Conda environment with:
+
+.. code-block:: bash
+
+   conda activate c12_callisto_clients
+
+And finally install the dependencies with
+
+.. code-block:: bash
+
+   poetry install
+
+Getting started
+====================================
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-   include_readme.rst
+
+   self
+
    0. README.ipynb
    1. Short_introduction_to_Qiskit.ipynb
    2. Using_the_C12_emulator.ipynb
@@ -26,6 +72,7 @@ We are so happy and proud to let you experiment with a software that imitates ne
    4. Using API.ipynb
    5. Getting mid circuit states.ipynb
    6. Physical parameters of the system.ipynb
+   7. Tket backend.ipynb
 
 
 Indices and tables
