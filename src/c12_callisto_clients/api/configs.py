656a63c8a16f41e230d548237b71dc9aa92c0f7f
rviktor85 <119857613+rviktor85@users.noreply.github.com>
2024-07-31 09:56:02 +0200

    change version yaml

--- src/c12_callisto_clients/api/configs.py
+++ src/c12_callisto_clients/api/configs.py
@@ -5,9 +5,9 @@
 import os
 
 
-HOST_URL = os.getenv("C12_HOST_URL", "57.128.103.232")
-PORT = os.getenv("C12_PORT", "8080")
-PROTOCOL = os.getenv("C12_PROTOCOL", "http")
+HOST_URL = os.getenv("C12_HOST_URL", "api.callisto.c12qe.net")
+PORT = os.getenv("C12_PORT", None)
+PROTOCOL = os.getenv("C12_PROTOCOL", "https")
 
 API_BASE_URL = f"{PROTOCOL}://{HOST_URL}{'' if PORT is None else ':'+str(PORT)}/api"
 
